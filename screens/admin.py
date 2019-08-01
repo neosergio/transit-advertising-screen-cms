c993d46d991d7b5b99203ccf3c4b4c66be225336
Sergio Infante <raulsergio9@gmail.com>
2019-07-31 21:32:07 -0500

    Add location and screen models

--- screens/admin.py
+++ screens/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Location, Screen
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class ScreenAdmin(admin.ModelAdmin):
+    list_display = ('url', 'is_active', 'location', 'priority', 'seconds', 'text', 'created_at', 'modified_at')
+    search_fields = ['text', 'location', 'url']
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Screen, ScreenAdmin)
--- screens/models.py
+++ screens/models.py
@@ -1,3 +1,28 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta(object):
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Screen(models.Model):
+    url = models.URLField(unique=True)
+    is_active = models.BooleanField(default=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    priority = models.PositiveSmallIntegerField(default=0)
+    seconds = models.PositiveSmallIntegerField(default=60)
+    text = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta(object):
+        ordering = ['-modified_at', '-created_at']
+
+    def __str__(self):
+        return self.text
