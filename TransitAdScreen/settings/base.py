2743e5953fc4a65cb0d4bf913f75de4a7afbdf5f
Sergio Infante <raulsergio9@gmail.com>
2019-08-08 12:44:09 -0500

    Separate local settings file

--- TransitAdScreen/settings/base.py
+++ TransitAdScreen/settings/base.py
@@ -16,18 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vqo@ek#s5f2@halrp3-z8rhl9&%^=$s(50vdg^dz@ccgj4plwj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,10 +64,7 @@ WSGI_APPLICATION = 'TransitAdScreen.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': {}
 }
 
 
---
+++ TransitAdScreen/settings/local.py
@@ -0,0 +1,26 @@
+"""
+DJANGO_SETTINGS_MODULE=TransitAdScreen.settings.local
+"""
+from .base import *  # noqa: 403
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'vqo@ek#s5f2@halrp3-z8rhl9&%^=$s(50vdg^dz@ccgj4plwj'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
